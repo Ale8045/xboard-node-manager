907810beeb8ea817dc1c4c2cf50d48ca515b8db4
Ale8045 <tutuh8618@gmail.com>
2026-06-12 05:24:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-
 set -e
 
-SCRIPT_URL="https://raw.githubusercontent.com/Ale8045/xboard-node-manager/main/xboard-node.sh"
+REPO="Ale8045/xboard-node-manager"
+SCRIPT_URL="https://raw.githubusercontent.com/${REPO}/main/xboard-node.sh"
 INSTALL_PATH="/usr/local/bin/xboard-node"
 
-echo "正在安装 Xboard Node Manager..."
-
 apt update -y
-apt install -y curl wget unzip socat cron tar
+apt install -y curl wget unzip tar cron lsof nano ca-certificates
 
 curl -fsSL "$SCRIPT_URL" -o "$INSTALL_PATH"
 chmod +x "$INSTALL_PATH"
 
 ln -sf "$INSTALL_PATH" /usr/bin/xnode
 
-echo
 echo "安装完成！"
-echo "启动命令：xnode"
-echo
-
+echo "以后输入 xnode 即可打开菜单"
 xnode
